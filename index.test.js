1fb9becbea091bdb42d12b13f1096e7b11a0c668
John Arocha <john.arocha@clover.com>
2024-11-13 08:31:52 -0700

    added tests for middleware

--- index.test.js
+++ index.test.js
@@ -1,12 +1,12 @@
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require('child_process')
+execSync('npm install')
+execSync('npm run seed')
 
 const request = require("supertest")
-const { db } = require('./db/connection');
+const { db } = require('./db/connection')
 const { Musician } = require('./models/index')
-const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+const app = require('./src/app')
+const {seedMusician} = require("./seedData")
 
 describe('./musicians endpoint', () => {
     // Write your tests here
@@ -28,5 +28,23 @@ describe('./musicians endpoint', () => {
     test('expect status code 200', async () => {
         const response = await request(app).delete('/musicians/1')
         expect(response.statusCode).toBe(200)
+    })
+
+    test('should return errors array when "name" field is empty', async () => {
+        const response = await request(app)
+            .post('/musicians')
+            .send({ instrument: 'Guitar' })
+    
+        expect(response.statusCode).toBe(400)
+        expect(response.body.errors).toBeDefined()
+    })
+
+    test('should return errors array when "instrument" field is empty', async () => {
+        const response = await request(app)
+            .post('/musicians')
+            .send({ name: 'John Doe' })
+
+        expect(response.statusCode).toBe(400)
+        expect(response.body.errors).toBeDefined()
     })
 })
